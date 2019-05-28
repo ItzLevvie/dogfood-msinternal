2e3f6d659a2178f8ff46f2e8d01f2309da37df14
ItzLevvie <rsajeey@hotmail.com>
2019-05-28 11:55:59 +0100

    Initial commit

---
+++ launch.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+https.get('https://mrodevicemgr.edog.officeapps.live.com/mrodevicemgrsvc/api/v2/C2RReleaseData?audienceFFN=ea4a4090-de26-49d7-93c1-91bff9e53fc3', (res) => {
+    res.on('data', (d) => {
+        let obj = JSON.parse(d);
+        let id = obj.FFN;
+        let ver = obj.AvailableBuild;
+
+        console.log(
+`\
+mkdir ClickToRun
+
+wget http://officecdn.microsoft.com.edgesuite.net/pr/${id}/Office/Data/${ver}/i640.cab -P ClickToRun > /dev/null 2>&1
+wget http://officecdn.microsoft.com.edgesuite.net/pr/${id}/Office/Data/${ver}/i641033.cab -P ClickToRun > /dev/null 2>&1
+
+cabextract ClickToRun/i640.cab ClickToRun/i641033.cab -d ClickToRun > /dev/null 2>&1
+rm ClickToRun/i640.cab ClickToRun/i641033.cab
+
+echo '@echo off' > launch.bat
+echo 'if not exist "%CommonProgramFiles%\\microsoft shared\\ClickToRun" (' >> launch.bat
+echo '    robocopy %~dp0ClickToRun "%CommonProgramFiles%\\microsoft shared\\ClickToRun" > nul 2>&1' >> launch.bat
+echo ')' >> launch.bat
+echo 'start "" "%CommonProgramFiles%\\microsoft shared\\ClickToRun\\OfficeClickToRun.exe" deliverymechanism=${id} platform=x64 culture=en-us productstoadd=O365ProPlusRetail.16_en-us_x-none O365ProPlusRetail.excludedapps.16=groove,onenote updatesenabled.16=True cdnbaseurl.16=http://officecdn.microsoft.com/pr/${id} version.16=${ver} mediatype.16=CDN baseurl.16=http://officecdn.microsoft.com/pr/${id}' >> launch.bat
+`
+        );
+    });
+});
