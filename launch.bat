f9ef8c340cfdde7ef2a930d5edea1677824e68cf
ItzLevvie <rsajeey@hotmail.com>
2019-10-23 17:49:34 +0100

    [testing] Update to latest Office versions

--- launch.bat
+++ launch.bat
@@ -90,46 +90,46 @@ echo.
 
 if %option% == 1 (
     set branch=ea4a4090-de26-49d7-93c1-91bff9e53fc3
-    set build=16.0.12208.20008
+    set build=16.0.12221.20012
 ) else if %option% == 2 (
     set branch=f3260cf1-a92c-4c75-b02e-d64c0a86a968
-    set build=16.0.12130.20144
+    set build=16.0.12130.20222
 ) else if %option% == 3 (
     set branch=834504cc-dc55-4c6d-9e71-e024d0253f6d
-    set build=16.0.11929.20392
+    set build=16.0.11929.20414
 ) else if %option% == 4 (
     set branch=c4a7726f-06ea-48e2-a13a-9d78849eb706
     set build=16.0.10730.20386
 ) else if %option% == 5 (
     set branch=b61285dd-d9f7-41f2-9757-8f61cba4e9c8
-    set build=16.0.12130.20112
+    set build=16.0.12215.20006
 ) else if %option% == 6 (
     set branch=5462eee5-1e97-495b-9370-853cd873bb07
-    set build=16.0.12130.20112
+    set build=16.0.12130.20210
 ) else if %option% == 7 (
     set branch=9a3b7ff2-58ed-40fd-add5-1e5158059d1c
-    set build=16.0.11929.20388
+    set build=16.0.11929.20396
 ) else if %option% == 8 (
     set branch=f4f024c8-d611-4748-a7e0-02b6e754c0fe
     set build=16.0.11328.20438
 ) else if %option% == 9 (
     set branch=1d2d2ea6-1680-4c56-ac58-a441c8c24ff9
-    set build=16.0.10351.20054
+    set build=16.0.10352.20003
 ) else if %option% == 10 (
     set branch=5440fd1f-7ecb-4221-8110-145efaa6372f
-    set build=16.0.12130.20112
+    set build=16.0.12215.20006
 ) else if %option% == 11 (
     set branch=64256afe-f5d9-4f86-8936-8840a6a4f5be
-    set build=16.0.12026.20320
+    set build=16.0.12130.20210
 ) else if %option% == 12 (
     set branch=b8f9b850-328d-4355-9145-c59439a0c4cf
-    set build=16.0.11929.20388
+    set build=16.0.11929.20396
 ) else if %option% == 13 (
     set branch=2e148de9-61c8-4051-b103-4af54baffbb4
     set build=16.0.10351.20054
 ) else if %option% == 14 (
     set branch=492350f6-3a01-4f97-b9c0-c7c6ddf67d60
-    set build=16.0.12026.20320
+    set build=16.0.12026.20344
 ) else if %option% == 15 (
     set branch=7ffbc6bf-bc32-4f92-8982-f9dd17fd3114
     set build=16.0.11328.20438
